7709feedb60cdc8293b95536e86a4b1f6fdb1f6d
pracniscate <lorena.giddens@outlook.com>
2018-12-01 19:38:01 -0500

    learn lesson

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root 'searches#search'
   get '/search', to: 'searches#search'
   post '/search', to: 'searches#foursquare'
-
+  # route the redirect to the SessionsController and create action,
+  # which is where we'll get the token
+  get '/auth', to: 'sessions#create'
+  get '/friends', to: 'searches#friends'
 end
